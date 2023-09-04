c3eb054293aa65b3ebea4ad772247fda7e03d65d
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2023-09-03 23:13:44 -0600

    Comprehensions
    List comprehensions
    Dictionary comprehensions
    Set comprehensions

--- Start/Ch_3/challenge_start.py
+++ Start/Ch_3/challenge_start.py
@@ -1,15 +1,30 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # Programming challenge for comprehensions
-
+#
 import string
 import pprint
 
-
+#CADENA A PROCESAR
 test_str = "2 apples, 9 oranges?, 4 pears, Mike's 1 egg, Jane's 2 kiwis, $50!"
 
 # YOUR CODE HERE
+#total de caracteres en el string
+l = len(test_str)
+#numero de caracteres
+nums = len([c for c in test_str if c.isnumeric()])
+#contar caracteres de puntuacion
+punctchars = len([c for c in test_str if c in string.punctuation])
+#contar los caracteres unicos
+unique = "".join({c for c in test_str if c.isalpha()})
 
 # print the data
+
+
 str_data = {
+    "Length":1,
+    "Digits":nums,
+    "Punctuation": punctchars,
+    "Unique letters":unique,
+    "Unique count": len(unique)    
 }
 pprint.pp(str_data)
--- Start/Ch_3/listcomp_start.py
+++ Start/Ch_3/listcomp_start.py
@@ -7,7 +7,12 @@ evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 # TODO: Perform a mapping and filter function on a list
-
+#event_square = list(map(lambda e:e**2, 
+#                       filter(lambda e: e> 4 and e <16, evens)))
+#print(event_square)
 # TODO: Derive a new list of numbers frm a given list
-
+event_square = [e**2 for e in evens] #-> La funcion eleva al cuadrado los pares de didhca lista de pares
+print(event_square)
 # TODO: Limit the items operated on with a predicate condition
+odd_square = [e**2 for e in odds if e > 3 and e <17] #la funcion eleva al cuadrado la lista de impares y aplica el filtro entrte 3 y 17
+print(odd_square) 
